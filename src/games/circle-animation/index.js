6646238a8dd08742a1347a995df6c1449119d7cc
xwillxu <hairblack088@gmail.com>
2022-04-03 17:18:06 -0400

    Circle Animation

---
+++ src/games/circle-animation/index.js
@@ -0,0 +1,12 @@
+var config = {
+    width: 800,
+    height: 600,
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    scene: {
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
