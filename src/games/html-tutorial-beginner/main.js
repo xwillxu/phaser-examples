1ec7056e753d5b1625db03a40babb9772df05c19
xwillxu <hairblack088@gmail.com>
2022-05-17 17:50:49 -0400

    Add Username Interaction

--- src/games/html-tutorial-beginner/main.js
+++ src/games/html-tutorial-beginner/main.js
@@ -1,5 +1,6 @@
 const h1Collection = document.getElementsByTagName('h1')
 const h1 = h1Collection[0]
+let title = 'Google is Cool, '
 
 const myCoolInterval = setInterval(function () {
     changeH1(h1, 'Xwill Hacked')
@@ -10,5 +11,26 @@ setTimeout(function () {
     clearInterval(myCoolInterval)
 }, 15000)
 function changeH1(target, content) {
-    target.textContent = content
+    target.textContent = content + ', ' + localStorage.getItem('name')
+    title = content + ', '
+}
+
+let button = document.querySelector('button')
+let heading = document.querySelector('h1')
+
+function setUserName() {
+    let name = prompt('Please Enter Name')
+    localStorage.setItem('name', name);
+    heading.textContent = title + name;
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName()
+} else {
+    let storagedName = localStorage.getItem('name')
+    heading.textContent = title + storagedName
+}
+console.log(button)
+button.onclick = function () {
+    setUserName()
 }
