fe24dbcd25e6b4fa201ce25a7b1376913fecc4df
xwillxu <hairblack088@gmail.com>
2022-06-14 18:12:16 -0400

    Learned Three Ways To Use Javascript In HTML

--- src/games/html-css-javascript-test/main.js
+++ src/games/html-css-javascript-test/main.js
@@ -1,8 +1,24 @@
-const para = document.querySelector('p')
 
-para.addEventListener('click', updateName)
+document.addEventListener('DOMContentLoaded', () => {
+    const para = document.querySelector('p')
 
-function updateName() {
-    const name = prompt('Enter Name')
-    para.textContent = `Player 1: ${name}`
-}
+    para.addEventListener('click', updateName)
+
+
+    function updateName() {
+        const name = prompt('Enter Name')
+        para.textContent = `Player 1: ${name}`
+    }
+
+    function createParagraph() {
+        const para = document.createElement('p');
+        para.textContent = 'You clicked the button!';
+        document.body.appendChild(para);
+    }
+
+    const buttons = document.querySelectorAll('button');
+
+    for (const button of buttons) {
+        button.addEventListener('click', createParagraph);
+    }
+});
