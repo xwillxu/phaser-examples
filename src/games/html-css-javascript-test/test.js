2d5170a05bc8d63fbaa95833b907d97bd7fe604e
xwillxu <hairblack088@gmail.com>
2022-06-18 10:53:37 -0400

    Almost finish the game where you can guess numbers and the Ai will give you hints

--- src/games/html-css-javascript-test/test.js
+++ src/games/html-css-javascript-test/test.js
@@ -1,7 +1,27 @@
 let inputs = []
 
-let answer = parseInt(Math.random() * 100) + 1
-console.log('answer', answer)
+let answer = 0
+
+let wonContainer = document.querySelector("#youWon")
+
+let restartButton = document.querySelector("#restart")
+
+restart()
+
+
+restartButton.onclick = (e) => {
+
+    restart()
+}
+
+function restart() {
+    answer = parseInt(Math.random() * 100) + 1
+    console.log('answer', answer)
+    restartButton.hidden = true
+    wonContainer.innerHTML = ''
+    inputs = []
+
+}
 
 // Send message to room on submit
 document.querySelector("#form").onsubmit = function (e) {
@@ -20,9 +40,33 @@ document.querySelector("#form").onsubmit = function (e) {
     console.log('nan', nanVarible)
     inputs.push(userAnswer)
 
+    let highOrLowContainer = document.querySelector('#highOrLow')
+
     if (userAnswer == answer) {
-        alert('You won')
+        let wonVarible = document.createElement('div')
+        wonContainer.innerHTML = ''
+        wonVarible.innerText = 'Congratulations! You got it right!'
+        restartButton.hidden = false
+        wonContainer.appendChild(wonVarible)
+    } else {
+        let wonVarible = document.createElement('div')
+        wonContainer.innerHTML = ''
+        wonVarible.innerText = 'Wrong!'
+        wonContainer.appendChild(wonVarible)
+
+        let hint = ''
+        if (userAnswer < answer) {
+            hint = 'You answer was too low.'
+        } else {
+            hint = 'You answer was too high.'
+        }
+        let highOrLowVarible = document.createElement('div')
+        highOrLowContainer.innerHTML = ''
+        highOrLowVarible.innerText = hint
+        highOrLowContainer.appendChild(highOrLowVarible)
     }
+
+
     let display = document.getElementById('answerDisplay')
     display.innerText = `Answer: ${inputs}`
 
