448ea05f91678b27a97344629ffd72c18c620f25
xwillxu <hairblack088@gmail.com>
2022-06-22 17:39:13 -0400

    Create some sort of exam to test your math skills

--- src/games/html-css-javascript-test/calculation.js
+++ src/games/html-css-javascript-test/calculation.js
@@ -2,17 +2,45 @@ let additionQuestion = document.querySelector('.addition')
 let subtractionQuestion = document.querySelector('.subtraction')
 let multiplicationQuestion = document.querySelector('.multiplication')
 let divisionQuestion = document.querySelector('.division')
-// let number1 = random(0, 100)
-// let number2 = random(0, 100)
-additionQuestion.textContent = ` 145 + 124 = ${calculateAddition(145, 124)}`
-subtractionQuestion.textContent = ` 935 - 210 = ${calculateSubtraction(935, 210)}`
-additionQuestion.textContent = ` 134 x 78 = ${calculateMultiplication(134, 78)}`
-additionQuestion.textContent = ` 659 ➗ 213 = ${calculateDivision(659, 213)}`
+
+let multiplicationAnswer;
+let multiplicationEquation;
+let questionMark = '❔❔❔'
+
+restart()
+
+let restartButton = document.querySelector('.restart')
+
+restartButton.onclick = () => {
+    restart()
+}
+
+multiplicationQuestion.onclick = () => {
+    multiplicationQuestion.textContent = `${multiplicationEquation}${multiplicationAnswer}`
+}
+
+function restart() {
+    let additionNumber1 = random(500) + 2
+    let additionNumber2 = random(500) + 2
+    additionQuestion.textContent = `${additionNumber1} +  ${additionNumber2} = ${calculateAddition(additionNumber1, additionNumber2)}`
+    let subtractionNumber1 = random(500) + 2
+    let subtractionNumber2 = random(500) + 2
+    subtractionQuestion.textContent = `${subtractionNumber1} - ${subtractionNumber2} = ${calculateSubtraction(subtractionNumber1, subtractionNumber2)}`
+    let multiplicationNumber1 = random(100) + 2
+    let multiplicationNumber2 = random(100) + 2
+    multiplicationAnswer = `${calculateMultiplication(multiplicationNumber1, multiplicationNumber2)}`
+    multiplicationEquation = `${multiplicationNumber1} x ${multiplicationNumber2} = `
+    multiplicationQuestion.textContent = `${multiplicationEquation}${questionMark}`
+    let divisionNumber1 = random(1000) + 2
+    let divisionNumber2 = random(500) + 2
+    divisionQuestion.textContent = `${divisionNumber1} ➗ ${divisionNumber2} = ${calculateDivision(divisionNumber1, divisionNumber2)}`
+
+}
 
 
 
-function random(number, range) {
-    number = Math.floor(Math.random() * range)
+function random(range) {
+    const number = Math.floor(Math.random() * range)
     return number
 }
 
@@ -36,11 +64,6 @@ function calculateDivision(number1, number2) {
     return answer
 }
 
-function calculateAdditionAndMultiplication(number1, number2, number3) {
-    let additionCalculation = calculateAddition(number1, number2)
-    let answer = calculateMultiplication(additionCalculation, number3)
-    return answer
-}
 
 function calculateEquation(baseNumber, numbers, equation) {
     let currentAnswer = baseNumber
