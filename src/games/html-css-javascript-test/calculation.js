cfd32c69b202ef9228f85db4605b78429b05571b
xwillxu <hairblack088@gmail.com>
2022-06-23 17:30:20 -0400

    Finish Making My Math Exam But There Is To Much Code.

--- src/games/html-css-javascript-test/calculation.js
+++ src/games/html-css-javascript-test/calculation.js
@@ -3,8 +3,18 @@ let subtractionQuestion = document.querySelector('.subtraction')
 let multiplicationQuestion = document.querySelector('.multiplication')
 let divisionQuestion = document.querySelector('.division')
 
+let additionAnswer;
+let additionEquation;
+
+let subtractionAnswer;
+let subtractionEquation;
+
 let multiplicationAnswer;
 let multiplicationEquation;
+
+let divisionAnswer;
+let divisionEquation
+
 let questionMark = '❔❔❔'
 
 restart()
@@ -15,25 +25,59 @@ restartButton.onclick = () => {
     restart()
 }
 
-multiplicationQuestion.onclick = () => {
+additionQuestion.onmousedown = () => {
+    additionQuestion.textContent = `${additionEquation}${additionAnswer}`
+}
+
+additionQuestion.onmouseup = () => {
+    additionQuestion.textContent = `${additionEquation}${questionMark}`
+}
+
+subtractionQuestion.onmousedown = () => {
+    subtractionQuestion.textContent = `${subtractionEquation}${subtractionAnswer}`
+}
+
+subtractionQuestion.onmouseup = () => {
+    subtractionQuestion.textContent = `${subtractionEquation}${questionMark}`
+}
+
+multiplicationQuestion.onmousedown = () => {
     multiplicationQuestion.textContent = `${multiplicationEquation}${multiplicationAnswer}`
+}
+
+multiplicationQuestion.onmouseup = () => {
+    multiplicationQuestion.textContent = `${multiplicationEquation}${questionMark}`
+}
+
+divisionQuestion.onmousedown = () => {
+    divisionQuestion.textContent = `${divisionEquation}${divisionAnswer}`
+}
+
+divisionQuestion.onmouseup = () => {
+    divisionQuestion.textContent = `${divisionEquation}${questionMark}`
 }
 
 function restart() {
     let additionNumber1 = random(500) + 2
     let additionNumber2 = random(500) + 2
-    additionQuestion.textContent = `${additionNumber1} +  ${additionNumber2} = ${calculateAddition(additionNumber1, additionNumber2)}`
-    let subtractionNumber1 = random(500) + 2
+    additionAnswer = `${calculateAddition(additionNumber1, additionNumber2)}`
+    additionEquation = `${additionNumber1} + ${additionNumber2} = `
+    additionQuestion.textContent = `${additionEquation}${questionMark}`
+    let subtractionNumber1 = random(1000) + 2
     let subtractionNumber2 = random(500) + 2
-    subtractionQuestion.textContent = `${subtractionNumber1} - ${subtractionNumber2} = ${calculateSubtraction(subtractionNumber1, subtractionNumber2)}`
+    subtractionAnswer = `${calculateSubtraction(subtractionNumber1, subtractionNumber2)}`
+    subtractionEquation = `${subtractionNumber1} - ${subtractionNumber2} = `
+    subtractionQuestion.textContent = `${subtractionEquation}${questionMark}`
     let multiplicationNumber1 = random(100) + 2
     let multiplicationNumber2 = random(100) + 2
     multiplicationAnswer = `${calculateMultiplication(multiplicationNumber1, multiplicationNumber2)}`
     multiplicationEquation = `${multiplicationNumber1} x ${multiplicationNumber2} = `
     multiplicationQuestion.textContent = `${multiplicationEquation}${questionMark}`
-    let divisionNumber1 = random(1000) + 2
+    let divisionNumber1 = random(5000) + 2
     let divisionNumber2 = random(500) + 2
-    divisionQuestion.textContent = `${divisionNumber1} ➗ ${divisionNumber2} = ${calculateDivision(divisionNumber1, divisionNumber2)}`
+    divisionAnswer = `${calculateDivision(divisionNumber1, divisionNumber2)}`
+    divisionEquation = `${divisionNumber1} ➗ ${divisionNumber2} = `
+    divisionQuestion.textContent = `${divisionEquation}${questionMark}`
 
 }
 
