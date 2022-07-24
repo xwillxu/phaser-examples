981f3b5dd9628b7443e8d4606b33ffab4b668f74
xwillxu <hairblack088@gmail.com>
2022-07-24 15:30:49 -0400

    Function Example Two

--- src/games/html-css-javascript-test/functionExamples/functionExampleTwo.js
+++ src/games/html-css-javascript-test/functionExamples/functionExampleTwo.js
@@ -1,24 +1,24 @@
-// const canvas = document.querySelector('canvas');
-// const ctx = canvas.getContext('2d');
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
 
-// const x = 50;
-// const y = 60;
-// const width = 100;
-// const height = 75;
-// const color = 'blue';
+const x = 50;
+const y = 60;
+const width = 100;
+const height = 75;
+const color = 'blue';
 
-// // Add your code here
-// let WIDTH = document.documentElement.clientWidth;
-// let HEIGHT = document.documentElement.clientHeight / 3
+// Add your code here
+let WIDTH = document.documentElement.clientWidth;
+let HEIGHT = document.documentElement.clientHeight / 3
 
-// function drawASquare(number) {
-//     ctx.clearRect(0, 0, WIDTH, HEIGHT);
-//     for (let i = 0; i < number; i++) {
-//         ctx.beginPath();
-//         ctx.fillStyle = `blue`;
-//         ctx.rect(x, y, width, height)
-//         ctx.fill();
-//     }
-// }
+function drawASquare(number) {
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+    for (let i = 0; i < number; i++) {
+        ctx.beginPath();
+        ctx.fillStyle = `blue`;
+        ctx.rect(x, y, width, height)
+        ctx.fill();
+    }
+}
 
-// drawASquare()
+drawASquare()
