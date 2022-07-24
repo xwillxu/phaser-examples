d52f41c86d59a8fe31b8ec21049f75b3375a4a77
xwillxu <hairblack088@gmail.com>
2022-07-24 15:16:06 -0400

    Make a scope example showing stuff about scope

---
+++ src/games/html-css-javascript-test/functionExamples/scopeExample.js
@@ -0,0 +1,71 @@
+// CASE 1: Works
+
+const x = 1;
+
+function a() {
+    const y = 2;
+    output(y);
+}
+
+function b() {
+    const z = 3;
+    output(z);
+}
+
+function output(value) {
+    const para = document.createElement('p');
+    document.body.appendChild(para);
+    para.textContent = `Value: ${value}`;
+}
+
+output(x)
+a()
+b()
+
+// CASE 2: Works
+
+const w = 1;
+
+function a() {
+    const s = 2;
+    output(w);
+}
+
+function b() {
+    const d = 3;
+    output(w);
+}
+
+function output(value) {
+    const para = document.createElement('p');
+    document.body.appendChild(para);
+    para.textContent = `Value: ${value}`;
+}
+
+output(x)
+a()
+b()
+
+// CASE 2: ERROR
+
+const arrowUp = 1;
+
+function a() {
+    const arrowRight = 2;
+    output(arrowLeft);
+}
+
+function b() {
+    const arrowLeft = 3;
+    output(arrowRight);
+}
+
+function output(value) {
+    const para = document.createElement('p');
+    document.body.appendChild(para);
+    para.textContent = `Value: ${value}`;
+}
+
+output(x)
+a()
+b()
