15751cef03380d1aa9d2492e1881f5476215ce25
xwillxu <hairblack088@gmail.com>
2022-08-04 18:19:58 -0400

    Amount of squares index

---
+++ src/games/-useful-stuff-/createMessage.js
@@ -0,0 +1,40 @@
+export default function displayMessage(msgText, msgType, msgName = 'User') {
+    const body = document.body
+
+    const panel = document.createElement('div')
+    panel.setAttribute('class', 'msgBox')
+    body.appendChild(panel)
+
+    const msg = document.createElement('p')
+    if (msgType == 'chat') {
+        msg.textContent = msgName + ': ' + msgText
+    } else if (msgType == 'warning') {
+        msg.textContent = 'WARNING: ' + msgText
+    } else {
+        msg.textContent = msgText
+    }
+
+    panel.appendChild(msg)
+
+    const closeBtn = document.createElement('button')
+    closeBtn.textContent = 'x'
+    panel.appendChild(closeBtn)
+
+    closeBtn.addEventListener('click', () => panel.parentNode.removeChild(panel))
+    switch (msgType) {
+        case 'warning':
+            msg.style.backgroundImage = 'url(https://raw.githubusercontent.com/mdn/learning-area/main/javascript/building-blocks/functions/icons/warning.png)'
+            panel.style.backgroundColor = 'red'
+            panel.style.width = "342px"
+            msg.style.textTransform = "uppercase";
+            msg.style.fontWeight = 'bolder'
+            break;
+        case 'chat':
+            msg.style.backgroundImage = 'url(https://raw.githubusercontent.com/mdn/learning-area/main/javascript/building-blocks/functions/icons/chat.png)'
+            panel.style.backgroundColor = 'green'
+            break
+        default:
+            msg.style.paddingLeft = '20px'
+            break;
+    }
+}
--- src/games/amount-of-squares/amountOfSquares.js
+++ src/games/amount-of-squares/amountOfSquares.js
@@ -1,8 +1,50 @@
+// // Import Stuff
+// import displayMessage from "../-useful-stuff-/createMessage.js"
 
+// // Get HTML Elements
+// const enterNumberBtn = document.querySelector('.enterButton')
+// const enterNumberText = document.querySelector('.typeNumber')
+// const answerQuestionBtn = document.querySelector('.answerQuestion')
+// const answerQuestionText = document.querySelector('.typeAnswer')
 
-function amountOfSquares(number) {
-    let result = 0
-    for (let x = 1; x <= number; x++) {
-        result += x * x
-    }
-}
+// // IMPORTANT: Div That Contains The Actual Squares That Are Generated
+// const squareContainer = document.querySelector('.squareContainer')
+
+// // Main Logic
+// let answer = 0
+
+// enterNumberBtn.addEventListener('click', () => {
+//     if (isNaN(enterNumberText.value)) {
+//         displayMessage("Please Only Type Numbers", "warning")
+//         return
+//     }
+//     answer = calculateSquares(enterNumberText.value)
+//     createSquares(enterNumberText.value)
+// })
+
+// // Calculation
+// function calculateSquares(number) {
+//     let result = 0
+//     for (let x = 1; x <= number; x++) {
+//         result += x * x
+//     }
+//     return result
+// }
+
+// // Create Squares
+// function createSquares(number) {
+//     squareContainer.innerHTML = ''
+//     const squareSideSize = (screen.height - 450) / number
+//     for (let x = 0; x < number; x++) {
+//         const row = document.createElement('div')
+//         row.className = 'row'
+//         squareContainer.appendChild(row)
+//         for (let y = 0; y < number; y++) {
+//             const column = document.createElement('div')
+//             column.className = 'square'
+//             column.style.width = squareSideSize + 'px'
+//             column.style.height = squareSideSize + 'px'
+//             row.appendChild(column)
+//         }
+//     }
+// }
