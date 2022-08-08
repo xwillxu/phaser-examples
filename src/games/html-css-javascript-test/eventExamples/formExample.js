104f46a7fa550fbeea92d3d3bd425fdcc58600e9
xwillxu <hairblack088@gmail.com>
2022-08-08 12:20:16 -0400

    Finish form example html

--- src/games/html-css-javascript-test/eventExamples/formExample.js
+++ src/games/html-css-javascript-test/eventExamples/formExample.js
@@ -1,15 +1,15 @@
-// const btn = document.querySelector('button');
-// const videoBox = document.querySelector('div');
+const btn = document.querySelector('button');
+const videoBox = document.querySelector('div');
 
-// function displayVideo() {
-//     if (videoBox.getAttribute('class') === 'hidden') {
-//         videoBox.setAttribute('class', 'showing');
-//     }
-// }
+function displayVideo() {
+    if (videoBox.getAttribute('class') === 'hidden') {
+        videoBox.setAttribute('class', 'showing');
+    }
+}
 
-// btn.addEventListener('click', displayVideo);
-// videoBox.addEventListener('click', () => videoBox.setAttribute('class', 'hidden'));
+btn.addEventListener('click', displayVideo);
+videoBox.addEventListener('click', () => videoBox.setAttribute('class', 'hidden'));
 
-// const video = document.querySelector('video');
+const video = document.querySelector('video');
 
-// video.addEventListener('click', () => video.play());
+video.addEventListener('click', () => video.play());
