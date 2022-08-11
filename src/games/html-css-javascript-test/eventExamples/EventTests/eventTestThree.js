17f83622d7fcd75bc2af700e0c35e1d67465987b
xwillxu <hairblack088@gmail.com>
2022-08-11 16:31:47 -0400

    Organize files

--- src/games/html-css-javascript-test/eventExamples/EventTests/eventTestThree.js
+++ src/games/html-css-javascript-test/eventExamples/EventTests/eventTestThree.js
@@ -1,7 +1,5 @@
 const buttonBar = document.querySelector('.button-bar');
 
-// Add your code here
-
 buttonBar.addEventListener('click', (event) => {
     event.target.style.backgroundColor = event.target.getAttribute('data-color')
 })
