f7a78423c3e0caccc395780c17c4a95324dda23d
xwillxu <hairblack088@gmail.com>
2022-08-12 09:03:52 -0400

    Get starting point for assesment

---
+++ src/games/html-css-javascript-test/Assesments/imageGallery/main.js
@@ -0,0 +1,18 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+
+/* Declaring the alternative text for each image file */
+
+/* Looping through images */
+
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+newImage.setAttribute('alt', xxx);
+thumbBar.appendChild(newImage);
+
+/* Wiring up the Darken/Lighten button */
