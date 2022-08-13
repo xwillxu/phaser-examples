063ff8c88f83e5b0d33a55d01b6ae40fbfa3d57e
xwillxu <hairblack088@gmail.com>
2022-08-13 09:18:31 -0400

    js

--- src/games/timer-countdown/timer.js
+++ src/games/timer-countdown/timer.js
@@ -1,21 +1,21 @@
-// // Create / Get HTML Elements
-// const container = document.createElement('div')
-// const htmlBody = document.querySelector('body')
-// const textbox = document.createElement('input')
-// const button = document.createElement('button')
+// Create / Get HTML Elements
+const container = document.createElement('div')
+const htmlBody = document.querySelector('body')
+const textbox = document.createElement('input')
+const button = document.createElement('button')
 
-// // Give Attributes / Style To Stuff
-// button.textContent = 'Enter'
-// textbox.placeholder = '30'
-// textbox.type = 'number'
+// Give Attributes / Style To Stuff
+button.textContent = 'Enter'
+textbox.placeholder = '30'
+textbox.type = 'number'
 
-// // Append Elements To Container
-// container.appendChild(textbox)
-// container.appendChild(button)
+// Append Elements To Container
+container.appendChild(textbox)
+container.appendChild(button)
 
-// for (const item of container.children) {
-//     item.className = 'flippedColor'
-// }
+for (const item of container.children) {
+    item.className = 'flippedColor'
+}
 
-// // Append Container To Body
-// htmlBody.appendChild(container)
+// Append Container To Body
+htmlBody.appendChild(container)
