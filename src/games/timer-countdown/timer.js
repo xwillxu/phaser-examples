b040382257006245fbced5e6f3e01f387b23cfcc
xwillxu <hairblack088@gmail.com>
2022-08-15 17:26:14 -0400

    Enhance timer to have hour and second

--- src/games/timer-countdown/timer.js
+++ src/games/timer-countdown/timer.js
@@ -6,7 +6,7 @@ export default function setupTimer() {
     // Create / Get HTML Elements
     const container = document.createElement('div')
     const htmlBody = document.querySelector('body')
-    const textbox = document.createElement('input')
+    const mintext = document.createElement('input')
     const button = document.createElement('button')
 
     // Get sound element and add sound
@@ -14,17 +14,24 @@ export default function setupTimer() {
     sound.src = '/sound/jingle-bells.mp3'
     sound.loop = true
 
+    // Extra text boxs and buttons    
+    const sectext = document.createElement('input')
+    const hourtext = document.createElement('input')
+
     // Timer Div
     const timerContainer = document.createElement('div')
 
     // Give Attributes / Style To Stuff
     button.textContent = 'Enter'
-    textbox.placeholder = '30'
-    textbox.type = 'number'
+    loopA(['placeholder', 'type'], [0, 'number'], [sectext, hourtext])
+    loopA(['placeholder', 'type'], [30, 'number'], [mintext])
 
     // Append Elements To Container
-    container.appendChild(textbox)
+    container.appendChild(hourtext)
+    container.appendChild(mintext)
+    container.appendChild(sectext)
     container.appendChild(button)
+
 
     for (const item of container.children) {
         item.className = 'flippedColor'
@@ -34,38 +41,57 @@ export default function setupTimer() {
     htmlBody.appendChild(container)
     htmlBody.appendChild(timerContainer)
 
+    // Loop function method
+    function loopM(method, methodAnswer, stuff) {
+
+    }
+    // Loop function attribute
+    function loopA(attributes, attributeAnswers, stuff) {
+        for (const item of stuff) {
+            let index = 0
+            for (const attribute of attributes) {
+                const answer = attributeAnswers[index]
+                item[attribute] = answer
+                index++;
+            }
+        }
+    }
+
     // Call function 1
     button.addEventListener('click', () => {
         clearTimeout(currentTimeout)
         sound.pause()
         sound.currentTime = 0
-
-        const text = Math.floor(Number(textbox.value))
-        if (text == 0) {
-            generateTimer("30")
-        } else if (text < 0) {
+        const mtext = Math.floor(Number(mintext.value))
+        const htext = Math.floor(Number(hourtext.value))
+        const stext = Math.floor(Number(sectext.value))
+        if (mtext < 0 || htext < 0 || stext < 0) {
             displayMessage('No negative numbers or 0', 'sorry')
+        } else if (mtext == 0 && htext == 0 && stext == 0) {
+            generateTimer(30)
         } else {
-            generateTimer(text)
+            generateTimer(mtext, htext, stext)
         }
-        textbox.value = ''
+        loopA(['value'], [''], [mintext, hourtext, sectext])
     })
 
     // Function 1: Generate Everything
-    function generateTimer(min, second = 0) {
+    function generateTimer(min, hour = 0, second = 0) {
         min = Math.floor(min)
-        const startTimerImage = min + ':' + String(second).padStart(2, '0')
+        hour = Math.floor(hour)
+        second = Math.floor(second)
+        const startTimerImage = hour + ':' + String(min).padStart(2, '0') + ':' + String(second).padStart(2, '0')
         const newPara = document.createElement('p')
         newPara.className = 'timer'
         newPara.textContent = startTimerImage
         timerContainer.innerHTML = ''
         timerContainer.appendChild(newPara)
-        const amountOfSeconds = min * 60 + second
-        startCountdown(min, second, amountOfSeconds)
+        const amountOfSeconds = hour * 3600 + min * 60 + second
+        startCountdown(min, hour, second, amountOfSeconds)
     }
 
     // Function 2: Start Countdown
-    function startCountdown(min, second, seconds) {
+    function startCountdown(min, hour, second, seconds) {
         currentTimeout = setTimeout(() => {
             if (seconds <= 0) {
                 sound.play()
@@ -77,11 +103,15 @@ export default function setupTimer() {
             } else if (second == 0) {
                 min -= 1
                 second = 59
+                if (min < 0) {
+                    hour -= 1
+                    min = 59
+                }
             } else {
-                second -= 59
-                seconds -= 59
+                second -= 1
+                seconds -= 1
             }
-            generateTimer(min, second)
+            generateTimer(min, hour, second)
         }, 1000)
     }
 }
