02b5a7353b97010f38dd5c3e140cdb3a876f2e05
xwillxu <hairblack088@gmail.com>
2022-08-22 15:06:18 -0400

    Create & finish test 4

--- src/games/html-css-javascript-test/objects/tests/test3.js
+++ src/games/html-css-javascript-test/objects/tests/test3.js
@@ -1,14 +1,20 @@
-function Cat(name, breed, color = 'white') {
-    this.name = name
-    this.breed = breed
-    this.color = color
-
-    this.greeting = function () {
-        console.log(`Hi, said ${this.name} the ${this.breed}`)
+const cat = {
+    name: 'Bertie',
+    breed: 'Cymric',
+    color: 'white',
+    greeting: function () {
+        console.log(`Hello, said ${this.name} the ${this.breed}.`);
     }
 }
 
-const cat = new Cat('Bertie', 'Cymric')
-cat.greeting()
-const cat2 = new Cat('Randy', 'Siberian Cat')
-cat2.greeting()
+const cat2 = {
+    name: 'Elfie',
+    breed: 'Aphrodite Giant',
+    color: 'ginger',
+    greeting: function () {
+        console.log(`Hello, said ${this.name} the ${this.breed}.`);
+    }
+}
+
+cat.greeting();
+cat2.greeting();
