83161f02b84b36a4ea53119db50d92242257331f
xwillxu <hairblack088@gmail.com>
2022-10-15 17:23:42 -0400

    Make sure each polygons hp is full

--- src/games/platformer/ContainerWithHealthBar.js
+++ src/games/platformer/ContainerWithHealthBar.js
@@ -3,19 +3,15 @@ import Phaser from 'phaser'
 import HealthBar from './HealthBar'
 
 export default class ContainerWithHealthBar extends Phaser.GameObjects.Container {
-    constructor(scene, x, y, children, offsetX = 40, offsetY = 50, size = 1) {
+    constructor(scene, x, y, children, offsetX = 40, offsetY = 50, size = 1, maxHp = 100) {
         super(scene, x, y, children)
         scene.add.existing(this);
         this.scene = scene;
-        this.hp = new HealthBar(scene, x, y, offsetX, offsetY, size);
+        this.hp = new HealthBar(scene, x, y, offsetX, offsetY, size, maxHp);
     }
 
     damage(amount) {
-        if (this.hp.decrease(amount)) {
-            return true
-        } else {
-            return false
-        }
+        this.hp.setHp(amount)
     }
 
     removeHp() {
