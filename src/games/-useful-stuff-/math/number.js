96a0015648499e29860d2bfc850b3857d5701674
xwillxu <hairblack088@gmail.com>
2023-01-19 18:54:59 -0500

    Improve useful suff

--- src/games/-useful-stuff-/math/number.js
+++ src/games/-useful-stuff-/math/number.js
@@ -1,8 +1,8 @@
 const special = ['zeroth', 'first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth', 'eleventh', 'twelfth', 'thirteenth', 'fourteenth', 'fifteenth', 'sixteenth', 'seventeenth', 'eighteenth', 'nineteenth'];
 const deca = ['twent', 'thirt', 'fort', 'fift', 'sixt', 'sevent', 'eight', 'ninet'];
 
-export function stringifyNumber(n) {
-    if (n < 20) return special[n];
-    if (n % 10 === 0) return deca[Math.floor(n / 10) - 2] + 'ieth';
-    return deca[Math.floor(n / 10) - 2] + 'y-' + special[n % 10];
+export function stringifyNumber(number) {
+    if (number < 20) return special[number];
+    if (number % 10 === 0) return deca[Math.floor(number / 10) - 2] + 'ieth';
+    return deca[Math.floor(number / 10) - 2] + 'y-' + special[number % 10];
 }
