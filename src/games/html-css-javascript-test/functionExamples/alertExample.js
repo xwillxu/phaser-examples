293351ff4fede87cdd5effc444d0d21fca30656d
xwillxu <hairblack088@gmail.com>
2023-03-04 11:04:35 -0500

    Update instructions

--- src/games/html-css-javascript-test/functionExamples/alertExample.js
+++ src/games/html-css-javascript-test/functionExamples/alertExample.js
@@ -1,4 +1,4 @@
-import displayMessage from "../../-useful-stuff-/createMessage";
+import displayMessage from "../../-useful-stuff-/user-inhancements/createMessage/createMessage";
 
 const btn = document.querySelector('button');
 btn.addEventListener('click', () => displayMessage('YOU ARE GETTING TO MANY MESSAGES YOU DEVICE IS ABOUT TO CRASH: BEST CASE. YOUR DEVICE HAS EXPLODED: Worst Case', 'warning'))
