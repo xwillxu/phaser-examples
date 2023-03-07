2cb83be7350c356f4701424fd6e6e7bbf8645cce
xwillxu <hairblack088@gmail.com>
2023-03-07 16:00:16 -0500

    Custom Prompt

---
+++ src/games/-useful-stuff-/user-inhancements/customPrompt/customPrompt.js
@@ -0,0 +1,6 @@
+const promptDiv = document.createElement("div")
+promptDiv.class = "hidden"
+promptDiv.id = "custom-prompt"
+const promptText = document.createElement("div")
+promptText.id = "prompt-text"
+promptDiv.append(promptText)
