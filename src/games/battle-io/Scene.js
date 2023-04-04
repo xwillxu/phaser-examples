c2f7bb1d3e42ce7b740e9d1744a36f6c677badb3
xwillxu <hairblack088@gmail.com>
2023-04-04 18:16:57 -0400

    Start adding movement stuff

--- src/games/battle-io/Scene.js
+++ src/games/battle-io/Scene.js
@@ -25,6 +25,8 @@ export default class Scene extends Phaser.Scene {
         this.name = ""
         // State for the keys
         this.keystate = {}
+        // Speed Multiplier
+        this.speedMultiplier = 1
         // The player's instuctions 1-11
         this.instructionsOne = displayMessage("Hello guest/user, welcome to battle.io.", "chat", "Battle.io")
         this.instructionsTwo = displayMessage("The objectives of the game are to fight enemies (both AI, and other guests/users), stay alive for as long as possible, and gain lots of EXP for you team. The team with the most score will win the game!", "chat", "Battle.io")
@@ -32,7 +34,7 @@ export default class Scene extends Phaser.Scene {
         this.instructionsFour = displayMessage("You start with a weapon that has mastery 1, to level up your mastery you gain EXP. The reason you want mastery is so that you can use special attacks. The Z special attack requires you to click once. While the X attack needs you hold it for some time before realeasing it.", "chat", "Battle.io")
         this.instructionsFive = displayMessage("You may change your weapon everytime you kill a enemy. Once you switch weapons you keep your mastery for you old weapon (just in case you want to switch back). Your new weapon will have mastery 1 or if you used it before you will get the old mastery.", "chat", "Battle.io")
         this.instructionsSix = displayMessage("There are currently three types of weapons, that each have three types of weapons in each catergory. These weapons in each catergory have different stats, but all belong to the same catergory. The three types of weapons are, swords, long ranged weapons, and abilitys. ", "chat", "Battle.io")
-        this.instructionsSixExtra = displayMessage("There are currently three affects for each ability. Light Affect: The speed multiplier is no longer 1 but 1.2. Fire Affect: Deals 8 damage over 2 seconds, if fire affect happens again within the 2 seconds the fire affect just lasts for a longer time. Wind Affect: The knockback multiplier is increased by 1.2, and there is sticky affect which slows down the enemy by a multiplier of 0.9.", "chat", "Battle.io")
+        this.instructionsSixExtra = displayMessage("There are currently three affects for each ability. Light Affect: The speed multiplier is no longer 1 but 1.2. Fire Affect: Deals 8 damage over 2 seconds, if fire affect happens again within the 2 seconds the fire affect just lasts for 2 extra seconds. Wind Affect: The knockback multiplier is increased by 1.2.", "chat", "Battle.io")
         this.instructionsSeven = displayMessage("Sword Catergory: [Double Iron Katana: [Damage: 15, Reload: 0.3secs, Range: 8, Knockback: 10], Emerald Sword: [Damage: 20, Reload: 0.4secs, Range: 7, Knockback: 9], Daggers: [Damage 6, Reload: 0.1secs, Range: 5, Knockback: 4]], Range Catergory: [Crossbow: [Damage: 50, Reload: 1.1secs, Speed: 35, Knockback: 14], Bazooka: [Damage: 99, Reload: 2.1secs, Speed: 25, Knockback: 30], Minigunner: [Damage: 4, Reload: 0.1secs, Speed: 30, Knockback: 2]]", "chat", "Battle.io")
         this.instructionsEight = displayMessage("Ability Catergory: [Light: Player uses the Light Bow: Damage: 20, Reload: 0.5secs, Speed: 50, Knockback: 14, Knockback: 8. Also the player with Light gets the Light Affect. Fire: Uses the Fire sword: Damage: 17, Reload: 0.4secs, Range: 7,  Knockback: 9. Also the player with Fire gets the Fire Affect. Wind: Player uses the Knockback Crossbow: Damage: 25, Reload: 0.7secs, Speed: 50, Knockback: 15. The player with wind also gets the Wind Affect.", "chat", "Battle.io")
         this.instructionsNine = displayMessage("There are currently three teams, you can actively join red team or blue team. The third team is the AI team, the amount of AIs depends on the mean of the player amount of the red team and blue team. The AI team can win and in that case all the players lost. When you win, you will basically become god and it's a free for all.", "chat", "Battle.io")
@@ -73,7 +75,7 @@ export default class Scene extends Phaser.Scene {
                 playerContainer.add(playerName)
 
                 this.players[sessionId] = playerContainer
-                player.onChange = //TODO: function
+                player.onChange = this //TODO: function 
 
             }
         })
@@ -154,15 +156,39 @@ export default class Scene extends Phaser.Scene {
 
     }
 
+    up() {
+        const speed = 7 * this.speedMultiplier
+        var movement = { y: speed }
+        this.room.send("move", movement);
+    }
+
+    left() {
+        const speed = -7 * this.speedMultiplier
+        var movement = { x: speed }
+        this.room.send("move", movement);
+    }
+
+    down() {
+        const speed = -7 * this.speedMultiplier
+        var movement = { y: speed }
+        this.room.send("move", movement);
+    }
+
+    right() {
+        const speed = 7 * this.speedMultiplier
+        var movement = { x: speed }
+        this.room.send("move", movement);
+    }
+
+
     sendToRoomAttackZ(fightingStyle, weapon, mastery) {
-        if (this.keystate.Z) {
-            const specialAttackZObject = {
-                fightingStyle: fightingStyle,
-                weapon: weapon,
-                mastery: mastery
-            }
-            this.room.send("specialAttackZ", specialAttackZObject)
+        const specialAttackZObject = {
+            fightingStyle: fightingStyle,
+            weapon: weapon,
+            mastery: mastery
         }
+        this.room.send("specialAttackZ", specialAttackZObject)
+
     }
 
 
@@ -179,17 +205,12 @@ export default class Scene extends Phaser.Scene {
 
     }
 
-    startTimerXMove(fightingStyle, weapon, mastery, eventHappens) {
-        if (!this.keystate.X) return
-
+    startTimerXMove(fightingStyle, weapon, mastery) {
         let time;
-
-
         const timeInterval = setInterval(() => {
             time += 0.1
         }, 100)
-
-        if (eventHappens) {
+        if (!this.keystate.X) {
             clearInterval(timeInterval)
             this.sendToRoomAttackX(fightingStyle, weapon, mastery, time)
         }
@@ -198,14 +219,34 @@ export default class Scene extends Phaser.Scene {
 
 
     create() {
-        const IDK = "I Don't Know"
         this.getUserName()
         this.connectToServer()
         this.setupKeys()
-        this.startTimerXMove(IDK, IDK, IDK, IDK)
     }
 
     update() {
-        //TODO: Update stuff
+        if (this.cursors.up.isDown || this.keystate.W == true) {
+            this.up()
+        }
+
+        if (this.cursors.left.isDown || this.keystate.A == true) {
+            this.left()
+        }
+
+        if (this.cursors.down.isDown || this.keystate.S == true) {
+            this.down()
+        }
+
+        if (this.cursors.right.isDown || this.keystate.D == true) {
+            this.right()
+        }
+
+
+        if (this.keystate.Z) {
+            this.sendToRoomAttackZ()
+        }
+        if (this.keystate.X) {
+            this.startTimerXMove()
+        }
     }
 }
