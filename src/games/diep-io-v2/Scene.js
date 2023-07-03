5ae0de40de85356301ef61eefe6b29ca9c912a02
xwillxu <hairblack088@gmail.com>
2023-07-03 14:07:12 -0400

    Create images for diep.io tanks

--- src/games/diep-io-v2/Scene.js
+++ src/games/diep-io-v2/Scene.js
@@ -5,6 +5,9 @@ import GUISceneUntouched from "./GUISceneUntouched.js"
 import ContainerWithHealthBar from "../platformer/ContainerWithHealthBar"
 import basicImage from "../../assets/images.png"
 import basicImage2 from "../../assets/Sniper.png"
+import basicImage3 from "../../assets/TwinFlank.png"
+import basicImage4 from "../../assets/Triangle.png"
+import basicImage5 from "../../assets/Booster.png"
 
 export default class Scene extends Phaser.Scene {
     constructor() {
@@ -44,6 +47,9 @@ export default class Scene extends Phaser.Scene {
     preload() {
         this.load.image("Basic", basicImage)
         this.load.image("Sniper", basicImage2)
+        this.load.image("Flank-Guard", basicImage3)
+        this.load.image("Tri-Angle", basicImage4)
+        this.load.image("Booster", basicImage5)
     }
 
     setupKeys() {
@@ -319,12 +325,12 @@ export default class Scene extends Phaser.Scene {
                     let container = new ContainerWithHealthBar(this, playerCircle.x, playerCircle.y, [], 77, -75, 2, playerCircle.hp);
                     let initialColor = 0xf04f54
                     // You will know that the playerCircle Belongs With This Player If The Players SessionId is The PlayerCircles PlayerId
-                    if (playerCircle.playerId == this.myId) initialColor = 0x00B1DE
+                    if (playerCircle.playerId != this.myId) this.myTankName = "Enemy" + this.myTankName
                     // Get tank attributes
                     const tankAttributes = this.tankInfo[this.myTankName]
 
                     const playerImage = this.add.sprite(0, 0, String(this.myTankName))
-                    playerImage.setScale(0.1, 0.1)
+                    playerImage.setScale(0.12, 0.12)
                     let text = this.add.text(0, 0, `${statePlayer?.name || "Guest"}`)
                     // const amountOfTurrets = this.tankInfo[this.myTankName]?.turrets
                     // for (let x = 0; x < tankAttributes?.turrets; x++) {
@@ -336,7 +342,8 @@ export default class Scene extends Phaser.Scene {
                     //     turret.setOrigin(-0.1, 0.5)
                     //     container.add(turret)
                     // }
-                    text.setOrigin(0.5, 0.5);
+                    text.setOrigin(0.5, 3.5);
+                    text.style.setShadow(2, 2, "black")
                     container.add(playerImage)
                     container.add(text)
                     this.listClients()
